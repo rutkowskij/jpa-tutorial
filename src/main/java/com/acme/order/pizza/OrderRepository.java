1b26bad70fd2dab0c4805f56a29d870dc5961a58
Jakub Rutkowski <jakub.rutkowski@comarch.com>
2015-07-09 10:25:14 +0200

    Spring data paging and searching configured

--- src/main/java/com/acme/order/pizza/OrderRepository.java
+++ src/main/java/com/acme/order/pizza/OrderRepository.java
@@ -2,10 +2,11 @@ package com.acme.order.pizza;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.acme.order.OrderStatus;
 
-public interface OrderRepository extends CrudRepository<PizzaOrder, Long> {
-	List<PizzaOrder> findByStatus(OrderStatus delivered);
+public interface OrderRepository extends JpaRepository<PizzaOrder, Long> {
+	List<PizzaOrder> findByStatus(@Param("status") OrderStatus status);
 }
