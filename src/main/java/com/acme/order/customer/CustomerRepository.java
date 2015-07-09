14087708c545b06d326524794b4c12e41a5f3f6c
Jakub Rutkowski <jakub.rutkowski@comarch.com>
2015-07-09 11:18:12 +0200

    CustomerType added

--- src/main/java/com/acme/order/customer/CustomerRepository.java
+++ src/main/java/com/acme/order/customer/CustomerRepository.java
@@ -1,6 +1,15 @@
 package com.acme.order.customer;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	@Query("select c from Customer c where c.type.name = 'INDIVIDUAL'")
+	List<Customer> findIndividual();
+
+	List<Customer> findByTypeName(@Param("name") String name);
 }
--- src/main/java/com/acme/order/customer/CustomerType.java
+++ src/main/java/com/acme/order/customer/CustomerType.java
@@ -5,34 +5,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Email;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "customer_t")
-public class Customer {
+@Table(name = "customer_type_t")
+public class CustomerType {
 	@Id
 	@GeneratedValue(strategy = GenerationType.TABLE)
 	private Long id;
 	@Column
 	private String name;
-	@Column
-	@Email
-	private String email;
-	@Column
-	private String address;
-
-	@ManyToOne
-	@JoinColumn(name = "type_id")
-	private CustomerType type;
-
 }
