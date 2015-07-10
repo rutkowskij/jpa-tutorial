1f4190fc935989a9900af649edd7871d9a0dd895
Jakub Rutkowski <jakub.rutkowski@comarch.com>
2015-07-10 10:52:52 +0200

    Sample repository method added

--- src/main/java/com/acme/order/customer/CustomerRepository.java
+++ src/main/java/com/acme/order/customer/CustomerRepository.java
@@ -1,6 +1,13 @@
 package com.acme.order.customer;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+	List<Customer> findByName(@Param("name") String name);
+
+	List<Customer> findByEmail(@Param("email") String email);
 }
